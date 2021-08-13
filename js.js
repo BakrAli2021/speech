ea24d41c37a18d034e078d37a2932f4e6ebf452a
BakrAli2021 <87548560+BakrAli2021@users.noreply.github.com>
2021-08-13 15:13:35 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,41 @@
+let input = document.querySelector('.inputbx input'),
+    btn = document.querySelector('.inputbx .icon'),
+    icon = document.querySelector('.inputbx .icon i');
+let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+if (!SpeechRecognition) {
+    alert("NOt Supported with this broweser please change to Chrome");
+} else {
+    let recognition = new SpeechRecognition();
+    btn.addEventListener("click", () => {
+        // icon.classList.toggle('fa-microphone-slash');
+        if (icon.classList.contains('fa-microphone')) {
+
+            recognition.start()
+
+        } else {
+
+            recognition.stop();
+        }
+
+    });
+    recognition.onstart = function() {
+        icon.classList.replace('fa-microphone', 'fa-microphone-slash');
+
+    }
+    recognition.onend = function() {
+        icon.classList.replace('fa-microphone-slash', 'fa-microphone');
+
+    }
+    recognition.addEventListener('result', function(event) {
+
+        let transcript = event.results[0][0].transcript;
+        input.value = transcript;
+        setTimeout(() => {
+            search(transcript);
+        }, 2000)
+    })
+}
+
+function search(searchvalue) {
+    window.location.href = `https://www.google.com/search?q=${searchvalue}`;
+}
